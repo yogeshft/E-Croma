6da4253cd3ae04063b043c42c814ed0a47648804
yogeshft <yadavyogesh404@gmail.com>
2023-06-30 20:02:43 +0530

    second-commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,22 @@ import Home from "./components/Home/Home";
 import Category from "./components/Category/Category";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
-// import AppContext from ". /utils/context";
+import AppContext from "./utils/context";
 
 function App() {
   return (
     <BrowserRouter>
+    <AppContext>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/category/:id" element={<Category />} />
-        <Route path="/product/id" element={<SingleProduct />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/product/id" element={<SingleProduct />} />
+        </Routes>
       <Newsletter />
       <Footer />
+    </AppContext>
+    
     </BrowserRouter>
   );
 }
--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,23 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png"
 const Banner = () => {
-    return <div>Banner</div>;
+    return <div className="hero-banner">
+        <div className="content">
+            <div className="text-content">
+                <h1>SALES</h1>
+                <p>
+                Convallis interdum purus adipiscing dis parturient
+                posuere ac a quam eleifend montes parturient posuere
+                curee tempor
+                </p>
+                <div className="ctas">
+                    <div  className="banner-cta">Read More</div>
+                    <div className="banner-cta v2">Shop Now</div>
+                </div>
+            </div>
+            <img className="banner-img" src={BannerImg} alt="" />
+        </div>
+    </div>;
 };
 
 export default Banner;
