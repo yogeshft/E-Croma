dfb2978f796f89bbf58a47a598a798533b3134b5
yogeshft <yadavyogesh404@gmail.com>
2023-06-30 14:41:51 +0530

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom" 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
@@ -7,24 +7,23 @@ import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
 // import AppContext from ". /utils/context";
 
-
 function App() {
-   return( 
-       <BrowserRouter>
-       <Header />
-    <Routes>
-        <Route path = "/" element = {<Home />}  />   
-        <Route path = "/category/:id" element = {<Category />}  />   
-        <Route path = "/product/id" element = {<SingleProduct />}  />   
-    </Routes>
-    <Newsletter />
-    <Footer />
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:id" element={<Category />} />
+        <Route path="/product/id" element={<SingleProduct />} />
+      </Routes>
+      <Newsletter />
+      <Footer />
     </BrowserRouter>
-    );
-
+  );
 }
 
 export default App;
 
- /* route contains 2 things i.e. path and element */ 
+/* route contains 2 things i.e. path and element */
 // number of pages in the web is equal to number of routes we have to create
+// hshdfjs
